436295eb1e84e85a08b8dd6d1bf1713d201a2a14
Chad Crissman <cdcrissman@gmail.com>
2013-11-19 13:27:11 -0500

    locations request specs

--- spec/requests/locations_spec.rb
+++ spec/requests/locations_spec.rb
@@ -1,11 +1,81 @@
 require 'spec_helper'
 
+def create_location
+  fill_in "Name", with: "Clearwater"
+  fill_in "Urn", with: "g5-cl-8cz7tip-clearwater"
+  click_button "Create Location"
+end
+
 describe "Locations" do
-  describe "GET /locations" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get locations_path
-      response.status.should be(200)
+  describe "Locations index" do
+    before do
+      visit locations_path
+    end
+
+    it "has locations heading" do
+      expect(page).to have_content "Locations"
+    end
+
+    it "shows all locations" do
+      click_link "New Location"
+      create_location
+      click_link "Pricing and Availability"
+      expect(page).to have_content "Clearwater"
+      expect(page).to have_content "g5-cl-8cz7tip-clearwater"
+    end
+  end
+
+  describe "New Location" do
+    before do
+      visit new_location_path
+    end
+
+    it "has new location heading" do
+      expect(page).to have_content "New Location"
+    end
+
+    it "lets me create a new location" do
+      create_location
+      expect(page).to have_content "Clearwater Units"
+    end
+  end
+
+  describe "Edit Locations" do
+    before do
+      visit new_location_path
+      create_location
+      click_link "Edit Location and Units"
+    end
+
+    it "has edit location heading" do
+      expect(page).to have_content "Edit Location"
+    end
+
+    it "has form filled in with location data" do
+      expect(page).to have_field("location[name]", with: "Clearwater")
+      expect(page).to have_field("location[urn]", with: "g5-cl-8cz7tip-clearwater")
+    end
+
+    it "can edit a location" do
+      fill_in "Name", with: "Tampa"
+      fill_in "Urn", with: "g5-cl-8cz7tip-tampa"
+      click_button "Update Location"
+      click_link "Pricing and Availability"
+      expect(page).to have_content "Tampa"
+      expect(page).to have_content "g5-cl-8cz7tip-tampa"
+    end
+  end
+
+  describe "Destroy Locations" do
+    it "can delete a location" do
+      visit new_location_path
+      create_location
+      click_link "Pricing and Availability"
+      click_link "Destroy"
+      expect(page).not_to have_content "Clearwater"
     end
   end
 end
+
+
+
