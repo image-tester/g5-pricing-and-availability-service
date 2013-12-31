7e89ef19fc4ed23fadf231e6f00f712b23cd0c37
Chad Crissman <cdcrissman@gmail.com>
2013-12-31 13:08:49 -0500

    adds link to check availability

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,7 +1,7 @@
 module LocationsHelper
   
   def pricing_service(floor_plan)
-    floor_plan.price_url ? floor_plan.price_url : "#"
+    floor_plan.price_url.blank? ? "#" : floor_plan.price_url
   end
   
 end
