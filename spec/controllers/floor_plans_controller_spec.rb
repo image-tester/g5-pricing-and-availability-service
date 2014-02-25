0cc1fc93a37827e44612c4838966baed49fe79e2
jessicard <jessicard@mac.com>
2014-02-24 18:16:11 -0800

    adds controller specs for floorplans when theres no http basic auth

--- spec/controllers/floor_plans_controller_spec.rb
+++ spec/controllers/floor_plans_controller_spec.rb
@@ -4,7 +4,6 @@ describe FloorPlansController do
 
   before(:each) do
     @location = Location.create! "urn" => "g5-cl-6cx7rin-hollywood", "name" => "Hollywood"
-    http_login
   end
 
   # This should return the minimal set of attributes required to create a valid
@@ -18,125 +17,191 @@ describe FloorPlansController do
   let(:valid_session) { {} }
 
   describe "GET index" do
-    it "assigns all floor_plans as @floor_plans" do
-      floor_plan = @location.floor_plans.create! valid_attributes
-      get :index, {location_id: @location.to_param}, valid_session
-      expect(assigns(:floor_plans)).to eq([floor_plan])
+    describe "with http basic auth" do
+      before :each do
+        http_login
+      end
+
+      it "assigns all floor_plans as @floor_plans" do
+        floor_plan = @location.floor_plans.create! valid_attributes
+        get :index, {location_id: @location.to_param}, valid_session
+        expect(assigns(:floor_plans)).to eq([floor_plan])
+      end
+
+      it "renders the locations index template" do
+        get :index, {location_id: @location.to_param}, valid_session
+        expect(response).to render_template(:index)
+      end
     end
 
-    it "renders the locations index template" do
-      get :index, {location_id: @location.to_param}, valid_session
-      expect(response).to render_template(:index)
+    describe "without http basic auth" do
+      it "doesnt render the locations index template" do
+        get :index, {location_id: @location.to_param}, valid_session
+        expect(response.status).to eq(401)
+      end
     end
-  end
 
-  describe "GET new" do
-    it "assigns a new floor_plan as @floor_plan" do
-      get :new, {location_id: @location.to_param}, valid_session
-      expect(assigns(:floor_plan)).to be_a_new(FloorPlan)
+    describe "GET new" do
+      describe "with http basic auth" do
+        before :each do
+          http_login
+        end
+
+        it "assigns a new floor_plan as @floor_plan" do
+          get :new, {location_id: @location.to_param}, valid_session
+          expect(assigns(:floor_plan)).to be_a_new(FloorPlan)
+        end
+      end
+
+      describe "without http basic auth" do
+        it "doesnt assign a new floor_plan as @floor_plan" do
+          get :new, {location_id: @location.to_param}, valid_session
+          expect(response.status).to eq(401)
+        end
+      end
     end
   end
 
   describe "POST create" do
-    describe "with valid params" do
-      it "creates a new FloorPlan" do
-        expect {
+    describe "with http basic auth" do
+      before :each do
+        http_login
+      end
+
+      describe "with valid params" do
+        it "creates a new FloorPlan" do
+          expect {
+            post :create, {:floor_plan => valid_attributes, location_id: @location.to_param}, valid_session
+          }.to change(FloorPlan, :count).by(1)
+        end
+
+        it "assigns a newly created floor_plan as @floor_plan" do
           post :create, {:floor_plan => valid_attributes, location_id: @location.to_param}, valid_session
-        }.to change(FloorPlan, :count).by(1)
+          expect(assigns(:floor_plan)).to be_a(FloorPlan)
+          expect(assigns(:floor_plan)).to be_persisted
+        end
+
+        it "redirects to the location page" do
+          post :create, {:floor_plan => valid_attributes, location_id: @location.to_param}, valid_session
+          response.should redirect_to(@location)
+          flash[:notice].should eq "Floor plan was successfully created."
+        end
+
+        it "does not raise an error" do
+          expect {post :create, :floor_plan => {title: "Cedar Point"}, location_id: @location.to_param}.not_to raise_error()
+        end
       end
 
-      it "assigns a newly created floor_plan as @floor_plan" do
-        post :create, {:floor_plan => valid_attributes, location_id: @location.to_param}, valid_session
-        expect(assigns(:floor_plan)).to be_a(FloorPlan)
-        expect(assigns(:floor_plan)).to be_persisted
-      end
+      describe "with invalid params" do
+        it "assigns a newly created but unsaved floor_plan as @floor_plan" do
+          # Trigger the behavior that occurs when invalid params are submitted
+          FloorPlan.any_instance.stub(:save).and_return(false)
+          post :create, {:floor_plan => { "title" => "invalid value" }, location_id: @location.to_param}, valid_session
+          expect(assigns(:floor_plan)).to be_a_new(FloorPlan)
+        end
 
-      it "redirects to the location page" do
-        post :create, {:floor_plan => valid_attributes, location_id: @location.to_param}, valid_session
-        response.should redirect_to(@location)
-        flash[:notice].should eq "Floor plan was successfully created."
-      end
-
-      it "does not raise an error" do
-        expect {post :create, :floor_plan => {title: "Cedar Point"}, location_id: @location.to_param}.not_to raise_error()
+        it "re-renders the 'new' template" do
+          # Trigger the behavior that occurs when invalid params are submitted
+          FloorPlan.any_instance.stub(:save).and_return(false)
+          post :create, {:floor_plan => { "title" => "invalid value" }, location_id: @location.to_param}, valid_session
+          expect(response).to render_template("new")
+        end
       end
     end
 
-    describe "with invalid params" do
-      it "assigns a newly created but unsaved floor_plan as @floor_plan" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        FloorPlan.any_instance.stub(:save).and_return(false)
-        post :create, {:floor_plan => { "title" => "invalid value" }, location_id: @location.to_param}, valid_session
-        expect(assigns(:floor_plan)).to be_a_new(FloorPlan)
-      end
-
-      it "re-renders the 'new' template" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        FloorPlan.any_instance.stub(:save).and_return(false)
-        post :create, {:floor_plan => { "title" => "invalid value" }, location_id: @location.to_param}, valid_session
-        expect(response).to render_template("new")
+    describe "without http basic auth" do
+      it "doesnt create a new FloorPlan" do
+        post :create, {:floor_plan => valid_attributes, location_id: @location.to_param}, valid_session
+        expect(response.status).to eq(401)
       end
     end
   end
 
   describe "PUT update" do
-    describe "with valid params" do
-      it "updates the requested floor_plan" do
-        floor_plan = @location.floor_plans.create! valid_attributes
-        # Assuming there are no other floor_plans in the database, this
-        # specifies that the FloorPlan created on the previous line
-        # receives the :update_attributes message with whatever params are
-        # submitted in the request.
-        FloorPlan.any_instance.should_receive(:update).with({ "title" => "MyFloorPlan" })
-        put :update, {:id => floor_plan.to_param, :floor_plan => { "title" => "MyFloorPlan" }, location_id: @location.to_param}, valid_session
+    describe "with http basic auth" do
+      before :each do
+        http_login
       end
 
-      it "assigns the requested floor_plan as @floor_plan" do
-        floor_plan = @location.floor_plans.create! valid_attributes
-        put :update, {:id => floor_plan.to_param, :floor_plan => valid_attributes, location_id: @location.to_param}, valid_session
-        expect(assigns(:floor_plan)).to eq(floor_plan)
+      describe "with valid params" do
+        it "updates the requested floor_plan" do
+          floor_plan = @location.floor_plans.create! valid_attributes
+          # Assuming there are no other floor_plans in the database, this
+          # specifies that the FloorPlan created on the previous line
+          # receives the :update_attributes message with whatever params are
+          # submitted in the request.
+          FloorPlan.any_instance.should_receive(:update).with({ "title" => "MyFloorPlan" })
+          put :update, {:id => floor_plan.to_param, :floor_plan => { "title" => "MyFloorPlan" }, location_id: @location.to_param}, valid_session
+        end
+
+        it "assigns the requested floor_plan as @floor_plan" do
+          floor_plan = @location.floor_plans.create! valid_attributes
+          put :update, {:id => floor_plan.to_param, :floor_plan => valid_attributes, location_id: @location.to_param}, valid_session
+          expect(assigns(:floor_plan)).to eq(floor_plan)
+        end
+
+        it "redirects to the location page" do
+          floor_plan = @location.floor_plans.create! valid_attributes
+          put :update, {:id => floor_plan.to_param, :floor_plan => valid_attributes, location_id: @location.to_param}, valid_session
+          expect(response).to redirect_to(@location)
+          flash[:notice].should eq "Floor plan was successfully updated."
+        end
       end
 
-      it "redirects to the location page" do
-        floor_plan = @location.floor_plans.create! valid_attributes
-        put :update, {:id => floor_plan.to_param, :floor_plan => valid_attributes, location_id: @location.to_param}, valid_session
-        expect(response).to redirect_to(@location)
-        flash[:notice].should eq "Floor plan was successfully updated."
+      describe "with invalid params" do
+        it "assigns the floor_plan as @floor_plan" do
+          floor_plan = @location.floor_plans.create! valid_attributes
+          # Trigger the behavior that occurs when invalid params are submitted
+          FloorPlan.any_instance.stub(:save).and_return(false)
+          put :update, {:id => floor_plan.to_param, :floor_plan => { "title" => "invalid value" }, location_id: @location.to_param}, valid_session
+          expect(assigns(:floor_plan)).to eq(floor_plan)
+        end
+
+        it "re-renders the 'edit' template" do
+          floor_plan = @location.floor_plans.create! valid_attributes
+          # Trigger the behavior that occurs when invalid params are submitted
+          FloorPlan.any_instance.stub(:save).and_return(false)
+          put :update, {:id => floor_plan.to_param, :floor_plan => { "title" => "invalid value" }, location_id: @location.to_param}, valid_session
+          expect(response).to render_template("edit")
+        end
       end
     end
 
-    describe "with invalid params" do
-      it "assigns the floor_plan as @floor_plan" do
+    describe "without http basic auth" do
+      it "updates the requested floor_plan" do
         floor_plan = @location.floor_plans.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
-        FloorPlan.any_instance.stub(:save).and_return(false)
-        put :update, {:id => floor_plan.to_param, :floor_plan => { "title" => "invalid value" }, location_id: @location.to_param}, valid_session
-        expect(assigns(:floor_plan)).to eq(floor_plan)
-      end
-
-      it "re-renders the 'edit' template" do
-        floor_plan = @location.floor_plans.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
-        FloorPlan.any_instance.stub(:save).and_return(false)
-        put :update, {:id => floor_plan.to_param, :floor_plan => { "title" => "invalid value" }, location_id: @location.to_param}, valid_session
-        expect(response).to render_template("edit")
+        put :update, {:id => floor_plan.to_param, :floor_plan => { "title" => "MyFloorPlan" }, location_id: @location.to_param}, valid_session
+        expect(response.status).to eq(401)
       end
     end
   end
 
   describe "DELETE destroy" do
-    it "destroys the requested floor_plan" do
-      floor_plan = @location.floor_plans.create! valid_attributes
-      expect {
+    describe "with http basic auth" do
+      before :each do
+        http_login
+      end
+
+      it "destroys the requested floor_plan" do
+        floor_plan = @location.floor_plans.create! valid_attributes
+        expect {
+          delete :destroy, {:id => floor_plan.to_param, location_id: @location.to_param}, valid_session
+        }.to change(FloorPlan, :count).by(-1)
+      end
+
+      it "redirects to the floor_plans list" do
+        floor_plan = @location.floor_plans.create! valid_attributes
         delete :destroy, {:id => floor_plan.to_param, location_id: @location.to_param}, valid_session
-      }.to change(FloorPlan, :count).by(-1)
+        expect(response).to redirect_to(@location)
+      end
     end
 
-    it "redirects to the floor_plans list" do
-      floor_plan = @location.floor_plans.create! valid_attributes
-      delete :destroy, {:id => floor_plan.to_param, location_id: @location.to_param}, valid_session
-      expect(response).to redirect_to(@location)
+    describe "without http basic auth" do
+      it "doesnt destroy the requested floor_plan" do
+        floor_plan = @location.floor_plans.create! valid_attributes
+        delete :destroy, {:id => floor_plan.to_param, location_id: @location.to_param}, valid_session
+        expect(response.status).to eq(401)
+      end
     end
   end
-
 end
