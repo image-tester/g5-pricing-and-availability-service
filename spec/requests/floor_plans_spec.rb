1c0c1df8e6e2b42ab39cf85180ed487f1f7c762e
jessicard <jessicard@mac.com>
2014-02-24 17:52:46 -0800

    adds request specs for floor plans when theres no http basic auth

--- spec/requests/floor_plans_spec.rb
+++ spec/requests/floor_plans_spec.rb
@@ -33,90 +33,142 @@ describe "Floor Plans" do
   end
 
   describe "Floor plans index" do
-    before do
-      http_login
-      create_location
-      create_floor_plan
-    end
-
-    it "has location floor plans heading" do
-      expect(page).to have_content "Clearwater Floor Plans"
-    end
-
-    it "shows all floor plans" do
-      expect(page).to have_content "Cedar Point"
-      expect(page).to have_content "3 Beds"
-    end
-
-    it "shows check availability link when none entered" do
-      expect(page).to have_content "Check Availability"
-    end
-
-    it "shows # available link when entered" do
-      click_link "Edit Location and Floor Plans"
-      within "#edit_floor_plan_1" do
-        fill_in "Available now", with: "2"
+    describe "with http basic auth" do
+      before do
+        http_login
+        create_location
+        create_floor_plan
       end
-      expect(page).to have_content "  Available"
+
+      it "has location floor plans heading" do
+        expect(page).to have_content "Clearwater Floor Plans"
+      end
+
+      it "shows all floor plans" do
+        expect(page).to have_content "Cedar Point"
+        expect(page).to have_content "3 Beds"
+      end
+
+      it "shows check availability link when none entered" do
+        expect(page).to have_content "Check Availability"
+      end
+
+      it "shows # available link when entered" do
+        click_link "Edit Location and Floor Plans"
+        within "#edit_floor_plan_1" do
+          fill_in "Available now", with: "2"
+        end
+        expect(page).to have_content "  Available"
+      end
+    end
+
+    describe "without http basic auth" do
+      before do
+        visit root_path
+      end
+
+      it "doesnt show all floor plans" do
+        expect(page).to have_content "HTTP Basic: Access denied."
+      end
     end
   end
 
   describe "New Floor Plan" do
-    before do
-      http_login
-      create_location
+    describe "with http basic auth" do
+      before do
+        http_login
+        create_location
+      end
+
+      it "has new floor plan heading" do
+        expect(page).to have_content "New Floor Plan"
+      end
+
+      it "lets me create a new floor plan" do
+        create_floor_plan
+        expect(page).to have_content "Cedar Point"
+        expect(page).to have_content "1400"
+      end
     end
 
-    it "has new floor plan heading" do
-      expect(page).to have_content "New Floor Plan"
-    end
+    describe "without http basic auth" do
+      before do
+        @location = Location.create! "urn" => "g5-cl-6cx7rin-hollywood", "name" => "Hollywood"
+        visit location_path(@location)
+      end
 
-    it "lets me create a new floor plan" do
-      create_floor_plan
-      expect(page).to have_content "Cedar Point"
-      expect(page).to have_content "1400"
+      it "doesnt let me create a new floor plan" do
+        click_link "Create New Floor Plan"
+        expect(page).to have_content "HTTP Basic: Access denied."
+      end
     end
   end
 
   describe "Edit Floor Plan" do
-    before do
-      http_login
-      create_location
-      create_floor_plan
-      click_link "Edit Location and Floor Plans"
+    describe "with http basic auth" do
+      before do
+        http_login
+        create_location
+        create_floor_plan
+        click_link "Edit Location and Floor Plans"
+      end
+
+      it "has edit floor plan header" do
+        expect(page).to have_content "Edit Floor Plans"
+      end
+
+      it "has form filled in with floor plan data" do
+        expect(page).to have_field("floor_plan[title]", with: "Cedar Point")
+        expect(page).to have_field("floor_plan[beds]", with: "3")
+      end
+
+      it "can edit a floor plan" do
+        fill_in "Title", with: "Mountain Ridge"
+        fill_in "Size", with: "1650"
+        click_button "Update Floor plan"
+        expect(page).to have_content "Mountain Ridge"
+        expect(page).to have_content "1650"
+      end
     end
 
-    it "has edit floor plan header" do
-      expect(page).to have_content "Edit Floor Plans"
-    end
+    describe "without http basic auth" do
+      before do
+        @location = Location.create! "urn" => "g5-cl-6cx7rin-hollywood", "name" => "Hollywood"
+        visit edit_location_path(@location)
+      end
 
-    it "has form filled in with floor plan data" do
-      expect(page).to have_field("floor_plan[title]", with: "Cedar Point")
-      expect(page).to have_field("floor_plan[beds]", with: "3")
-    end
-
-    it "can edit a floor plan" do
-      fill_in "Title", with: "Mountain Ridge"
-      fill_in "Size", with: "1650"
-      click_button "Update Floor plan"
-      expect(page).to have_content "Mountain Ridge"
-      expect(page).to have_content "1650"
+      it "cannot edit a floor plan" do
+        expect(page).to have_content "HTTP Basic: Access denied."
+      end
     end
   end
 
   describe "Destory Floor Plans" do
-    before do
-      http_login
+    describe "with http basic auth" do
+      before do
+        http_login
+      end
+
+      it "can destroy a floor plan" do
+        create_location
+        create_floor_plan
+        click_link "Edit Location and Floor Plans"
+        within(".edit_floor_plan") do
+          click_link "Destroy"
+        end
+        expect(page).not_to have_content "Cedar Point"
+      end
     end
 
-    it "can destroy a floor plan" do
-      create_location
-      create_floor_plan
-      click_link "Edit Location and Floor Plans"
-      within(".edit_floor_plan") do
-        click_link "Destroy"
+    describe "without http basic auth" do
+      before do
+        @location = Location.create! "urn" => "g5-cl-6cx7rin-hollywood", "name" => "Hollywood"
+        visit edit_location_path(@location)
       end
-      expect(page).not_to have_content "Cedar Point"
+
+      it "cannot destroy a floor plan" do
+        expect(page).to have_content "HTTP Basic: Access denied."
+      end
     end
   end
 
